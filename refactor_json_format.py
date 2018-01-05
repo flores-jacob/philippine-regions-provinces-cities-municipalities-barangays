576ef3e26a3dacae69220a3d0e13ccfe530a0c58
phwebmachine <phwebmachine@gmail.com>
2018-01-05 13:44:04 +0800

    refactor: Sort province and municipality names alphabetically

--- refactor_json_format.py
+++ refactor_json_format.py
@@ -5,10 +5,11 @@
 # In effect, only dictionaries are used all throughout the file, except
 # for the barangays which are still in list format
 
-
+from collections import OrderedDict
 import json
 
 JSON_FILE = "./philippine_provinces_cities_municipalities_and_barangays_2016.json"
+NEW_JSON_FILE = "./philippine_provinces_cities_municipalities_and_barangays_2016_v2.json"
 
 with open(JSON_FILE) as json_file:
     data = json.load(json_file)
@@ -19,12 +20,21 @@ for region_key, region_contents in data.items():
     modified_dict[region_key] = {}
     modified_dict[region_key]["region_name"] = region_contents["region_name"]
     modified_dict[region_key]["province_list"] = {}
-    for province_key, province_contents in region_contents["province_list"].items():
-        modified_dict[region_key]["province_list"][province_key] = {}
-        modified_dict[region_key]["province_list"][province_key]["municipality_list"] = {}
+    modified_province_list = modified_dict[region_key]["province_list"]
+    province_dict = region_contents["province_list"]
+    for province_key, province_contents in province_dict.items():
+        modified_province_list[province_key] = {}
+        modified_province_list[province_key]["municipality_list"] = {}
+        modified_municipality_list = modified_province_list[province_key]["municipality_list"]
         for municipality_item in province_contents["municipality_list"]:
-            for municipality_key, municipality_contents in municipality_item.items():
-                modified_dict[region_key]["province_list"][province_key]["municipality_list"][municipality_key] = municipality_contents
+            for municipality_key, municipality_contents in sorted(municipality_item.items(), key=lambda x: x[0]):
+                modified_municipality_list[municipality_key] = municipality_contents
 
-with open(JSON_FILE, "w") as outfile:
+        modified_dict[region_key]["province_list"][province_key]["municipality_list"] = OrderedDict(sorted(modified_municipality_list.items(), key=lambda x: x[0]))
+
+    modified_dict[region_key]["province_list"] = OrderedDict(sorted(modified_province_list.items(), key=lambda x: x[0]))
+
+modified_dict = OrderedDict(sorted(modified_dict.items(), key=lambda x: x[0]))
+
+with open(NEW_JSON_FILE, "w") as outfile:
     json.dump(modified_dict, outfile, indent=2)
